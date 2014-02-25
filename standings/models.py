8cc972b59e01bd9a4ef0f881439ef07c9ce0cf2c
Kalrogin <Crixsnrdr@gmail.com>
2014-02-25 15:53:30 -0500

    Adding models

--- standings/models.py
+++ standings/models.py
@@ -17,15 +17,33 @@ class CharacterInfo(db.Model):
     securityStatus =db.Column(db.Float, unique=False)
 
 class ContactList(db.Model):
-    created =db.Column(db.DateTime, unique=False)
-    modified =db.Column(db.DateTime, unique=False)
-    contactID =db.Column(db.Integer, primary_key=True)
-    contactName =db.Column(db.Text, unique=True)
-    standing =db.Column(db.Integer, unique=False)
+    created = db.Column(db.DateTime, unique=False)
+    modified = db.Column(db.DateTime, unique=False)
+    contactID = db.Column(db.Integer, primary_key=True)
+    contactName = db.Column(db.Text, unique=True)
+    standing = db.Column(db.Integer, unique=False)
 
+class WalletJournal(db.Model):
+    date = db.Column(db.DateTime, unique=False)
+    refID = db.Column(db.Integer, unique=True)
+    refTypeID = db.Column(db.Integer, unique=False)
+    ownerName1 = db.Column(db.Text, unique=False)
+    ownerID1 = db.Column(db.Integer, unique=False)
+    ownerName2 = db.Column(db.Text, unique=False)
+    ownerID2 = db.Column(db.Integer, unique=False)
+    argName1 = db.Column(db.Text, unique=False)
+    argID1 = db.Column(db.Integer, unique=False)
+    amount = db.Column(db.Float, unique=False)
+    balance = db.Column(db.Float, unique=False)
+    reason = db.Column(db.Text, unique=False)
+    owner1TypeID = db.Column(db.Integer, unique=False)
+    owner2TypeID = db.Column(db.Integer, unique=False)
+    wallet = db.Column(db.Integer, unique=False)
 
-
-
+class Notes(db.Model):
+    contactID = db.Column(db.Text, unique=False)
+    note = db.Column(db.Text, unique=False)
+    added = db.Column(db.Text, unique=False)
 
 
 
